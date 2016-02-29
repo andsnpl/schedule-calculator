13fc1f66b8faf417934751457ae5350cce0e0a19
B Paul Anderson <benpaul.anderson@gmail.com>
2016-02-28 22:33:48 -0600

    Improve error handling in userSessionData.sync

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,6 @@ import './components';
 
 import '../scss/index.scss';
 // not building the service worker as an entry point because it will fail to
-// execute due to the ektra code inserted by webpack
+// execute due to the extra code inserted by webpack
 import 'file?name=service-worker.js!./service-worker.js';
 import 'file?name=manifest.json!../manifest.json';
